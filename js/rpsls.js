a2f1d92b831719d9462313c70477fab338c2dc95
unknown <you@example.com>
2015-12-09 20:07:59 -0500

    added start game button

--- js/rpsls.js
+++ js/rpsls.js
@@ -7,90 +7,110 @@ $(document).ready(function() {
     var roundLost = 0;
     var tiedRounds = 0;
 
-    $(".shoot").on("click", function() {
-        var choiceRPS = ['rock', 'paper', 'scissor', 'lizard', 'spock'];
-        var ranNum = Math.floor(Math.random() * choiceRPS.length);
-        var compChoice = choiceRPS[ranNum];
-        var userChoice = this.id;
-        shoot++;
-        $("#shoot").html(shoot);
-        var compChoice = choiceRPS[ranNum];
-        $("#computerChoice").html(compChoice);
-        console.log(userChoice);
-        console.log(compChoice);
+    $("#toggleGame").on("click",function(){
+        if($(this).attr("data-status") === "off") {
+        $(this)
+            .addClass("btn-success")
+            .removeClass("btn-danger")
+            .attr("data-status", "on");
+    } else {
+        $(this)
+          .addClass("btn-danger")
+          .removeClass("btn-success")
+          .attr("data-status", "off");
+        bindControls();
+    }
+    })
 
-        if (compChoice == userChoice) {};
+    function bindControls() {
+        $(".shoot").on("click", function() {
+            var choiceRPS = ['rock', 'paper', 'scissor', 'lizard', 'spock'];
+            var ranNum = Math.floor(Math.random() * choiceRPS.length);
+            var compChoice = choiceRPS[ranNum];
+            var userChoice = this.id;
+            shoot++;
+            $("#shoot").html(shoot);
+            var compChoice = choiceRPS[ranNum];
+            $("#computerChoice").html(compChoice);
+            $("#yourChoice").html(userChoice);
+            console.log(userChoice);
+            console.log(compChoice);
 
-        if (userChoice === "rock") {
-            if (compChoice === "lizard" || compChoice === "scissor") {
-                yourScore++;
-            } else {
-                if (compChoice === "paper" || compChoice === "spock") {
-                    compScore++;
+            if (compChoice == userChoice) {};
+
+            if (userChoice === "rock") {
+                if (compChoice === "lizard" || compChoice === "scissor") {
+                    yourScore++;
+                } else {
+                    if (compChoice === "paper" || compChoice === "spock") {
+                        compScore++;
+                    }
                 }
-            }
-        };
-        if (userChoice === "paper") {
-            if (compChoice === "rock" || compChoice === "spock") {
-                yourScore++;
-            } else {
-                if (compChoice === "scissor" || compChoice === "lizard") {
-                    compScore++;
-                }
-            }
-        };
-        if (userChoice === "scissor") {
-            if (compChoice === "paper" || compChoice === "lizard") {
-                yourScore++;
-            } else {
-                if (compChoice === "rock" || compChoice === "spock") {
-                    compScore++;
-                }
-            }
-        };
-        if (userChoice === "lizard") {
-            if (compChoice === "spock" || compChoice === "paper") {
-                yourScore++;
-            } else {
-                if (compChoice === "rock" || compChoice === "scissor") {
-                    compScore++;
-                }
-            }
-        };
-        if (userChoice === "spock") {
-            if (compChoice === "rock" || compChoice === "scissor") {
-                yourScore++;
-            } else {
-                if (compChoice === "lizard" || compChoice === "paper") {
-                    compScore++;
-                }
-            }
-        };
-        $("#yourScore").html(yourScore);
-        $("#computerScore").html(compScore);
-        if (shoot === 5) {
-            round ++;
-            $("#round").html(round);
-            if (yourScore > compScore) {
-                roundWon ++;
-                $("#roundWon").html(roundWon);
-                $("#win").modal({
-                    keyboard:false
-                });
-            } else if (yourScore < compScore) {
-                roundLost ++;
-                $("#roundLost").html(roundLost);
-                $("#lose").modal({
-                    keyboard:false
-                });
-            } else if (yourScore == compScore) {
-                tiedRounds ++;
-                $("#tiedRounds").html(tiedRounds);
-                $("#tie").modal({
-                    keyboard:false
-                });
             };
-            shoot = yourScore = compScore = 0;
-        };
-    });
+            if (userChoice === "paper") {
+                if (compChoice === "rock" || compChoice === "spock") {
+                    yourScore++;
+                } else {
+                    if (compChoice === "scissor" || compChoice === "lizard") {
+                        compScore++;
+                    }
+                }
+            };
+            if (userChoice === "scissor") {
+                if (compChoice === "paper" || compChoice === "lizard") {
+                    yourScore++;
+                } else {
+                    if (compChoice === "rock" || compChoice === "spock") {
+                        compScore++;
+                    }
+                }
+            };
+            if (userChoice === "lizard") {
+                if (compChoice === "spock" || compChoice === "paper") {
+                    yourScore++;
+                } else {
+                    if (compChoice === "rock" || compChoice === "scissor") {
+                        compScore++;
+                    }
+                }
+            };
+            if (userChoice === "spock") {
+                if (compChoice === "rock" || compChoice === "scissor") {
+                    yourScore++;
+                } else {
+                    if (compChoice === "lizard" || compChoice === "paper") {
+                        compScore++;
+                    }
+                }
+            };
+            $("#yourScore").html(yourScore);
+            $("#computerScore").html(compScore);
+            if (shoot === 5) {
+                round ++;
+                $("#round").html(round);
+                $("h1").empty();
+                $(".clear").empty();
+                if (yourScore > compScore) {
+                    roundWon ++;
+                    $("#roundWon").html(roundWon);
+                    $("#win").modal({
+                        keyboard:false
+                    });
+                } else if (yourScore < compScore) {
+                    roundLost ++;
+                    $("#roundLost").html(roundLost);
+                    $("#lose").modal({
+                        keyboard:false
+                    });
+                } else if (yourScore == compScore) {
+                    tiedRounds ++;
+                    $("#tiedRounds").html(tiedRounds);
+                    $("#tie").modal({
+                        keyboard:false
+                    });
+                };
+                shoot = yourScore = compScore = 0;
+            };
+        });
+    };
 });
